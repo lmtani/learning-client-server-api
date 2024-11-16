3ab667bdf308d42eb6d9c396eed90caadad3e318
Lucas Taniguti <lucas.biotec07@gmail.com>
2024-11-16 18:21:08 -0300

    refactor(Server): Move structs to dedicated package

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -14,26 +14,6 @@ import (
 
 const CotacaoRoute = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 
-// CurrencyExchange represents the structure of the exchange rate data
-type CurrencyExchange struct {
-	UsdBrl UsdBrl `json:"USDBRL"`
-}
-
-// UsdBrl represents the details of the USD/BRL exchange rate
-type UsdBrl struct {
-	Code       string `json:"code"`
-	CodeIn     string `json:"codein"`
-	Name       string `json:"name"`
-	High       string `json:"high"`
-	Low        string `json:"low"`
-	VarBid     string `json:"varBid"`
-	PctChange  string `json:"pctChange"`
-	Bid        string `json:"bid"`
-	Ask        string `json:"ask"`
-	Timestamp  string `json:"timestamp"`
-	CreateDate string `json:"create_date"`
-}
-
 func main() {
 	http.HandleFunc("/cotacao", func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(r.Context(), 200*time.Millisecond)
@@ -65,7 +45,7 @@ func main() {
 			return
 		}
 
-		var exchange CurrencyExchange
+		var exchange entities.CurrencyExchange
 		if err := json.Unmarshal(data, &exchange); err != nil {
 			http.Error(w, "Error unmarshalling JSON", http.StatusInternalServerError)
 			return
--- internal/entities/entities.go
+++ internal/entities/entities.go
@@ -3,3 +3,23 @@ package entities
 type Cotacao struct {
 	Bid string `json:"bid"`
 }
+
+// CurrencyExchange represents the structure of the exchange rate data
+type CurrencyExchange struct {
+	UsdBrl UsdBrl `json:"USDBRL"`
+}
+
+// UsdBrl represents the details of the USD/BRL exchange rate
+type UsdBrl struct {
+	Code       string `json:"code"`
+	CodeIn     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
